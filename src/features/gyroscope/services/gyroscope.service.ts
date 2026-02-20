232500501e5d246c930aeb13099ef294005b2eb7
camilo lehue <clehue@ast.cl>
2026-02-20 18:33:44 -0300

    fix: corrige las rutas de las solicitudes en los servicios de dangerZone, gyroscope, sismos y sismosNotificaciones

--- src/features/gyroscope/services/gyroscope.service.ts
+++ src/features/gyroscope/services/gyroscope.service.ts
@@ -3,7 +3,7 @@ import type { GyroscopeTypes } from "@/features/gyroscope/types/gyroscope.type";
 
 export const gyroscopeService = {
   getGyroscope: async (): Promise<GyroscopeTypes> => {
-    const response = await apiSystem.get<GyroscopeTypes>(`/gyroscope`);
+    const response = await apiSystem.get<GyroscopeTypes>(`/gyroscope/`);
     return response.data;
   },
 };
--- src/features/sismos/services/sismos.service.ts
+++ src/features/sismos/services/sismos.service.ts
@@ -3,7 +3,7 @@ import { apiSystem } from "@/apis/apiSystem";
 
 export const sismosService = {
   getSismos: async (): Promise<SismoTypes> => {
-    const response = await apiSystem.get<SismoTypes>(`/sismos`);
+    const response = await apiSystem.get<SismoTypes>(`/sismos/`);
     return response.data;
   },
 };
